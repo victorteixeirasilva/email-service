166a290159f0e9f41b4d25407428e50f59017e65
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-26 22:40:48 -0300

    feat: Protegendo Credenciais em Variaveis de Ambiente

--- src/main/java/com/inovasoft/ms/emailService/infra/SmtpConfig.java
+++ src/main/java/com/inovasoft/ms/emailService/infra/SmtpConfig.java
@@ -9,21 +9,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SmtpConfig {
 
+    @Value("${email.username}")
+    private String emailUsername;
+
+    @Value("${email.password}")
+    private String emailPassword;
+
     @Bean
     public JavaMailSender mailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//        mailSender.setHost("smtp.hostinger.com");
-//        mailSender.setPort(465);
-//        mailSender.setUsername("adm@inovasoft.tech");
-//        mailSender.setPassword("Vi$230803");
-//        mailSender.setProtocol("SMTPS");
-//        mailSender.getJavaMailProperties().put("mail.transport.protocol", "smtps");
-//        mailSender.getJavaMailProperties().put("mail.smtp.auth", "true");
-//        mailSender.getJavaMailProperties().put("mail.smtp.ssl.enable", "true");
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587); // Use 587 para TLS ou 465 para SSL
-        mailSender.setUsername("victorteixeira2308@gmail.com");
-        mailSender.setPassword("bhgc zctq dsnv zhul");
+        mailSender.setUsername(emailUsername);
+        mailSender.setPassword(emailPassword);
         mailSender.getJavaMailProperties().put("mail.transport.protocol", "smtp");
         mailSender.getJavaMailProperties().put("mail.smtp.auth", "true");
         mailSender.getJavaMailProperties().put("mail.smtp.starttls.enable", "true"); // Necessário para TLS
--- src/main/java/com/inovasoft/ms/emailService/infra/SmtpEmailService.java
+++ src/main/java/com/inovasoft/ms/emailService/infra/SmtpEmailService.java
@@ -2,6 +2,7 @@ package com.inovasoft.ms.emailService.infra;
 
 import com.inovasoft.ms.emailService.adapters.EmailSanderGateway;
 import com.inovasoft.ms.emailService.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ public class SmtpEmailService implements EmailSanderGateway {
 
     private final JavaMailSender mailSender;
 
+    @Value("${email.username}")
+    private String emailUsername;
+
     @Autowired
     public SmtpEmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
@@ -20,7 +24,7 @@ public class SmtpEmailService implements EmailSanderGateway {
     @Override
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("victorteixeira2308@gmail.com");
+        message.setFrom(emailUsername);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
