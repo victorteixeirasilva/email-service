182ba65051b48dcd749f878756f78ad7c1cfd585
Victor Teixeira Silva <105030970+victorteixeirasilva@users.noreply.github.com>
2026-01-09 18:28:33 -0300

    Enable Feign clients in EmailServiceApplication

--- src/main/java/com/inovasoft/ms/emailService/EmailServiceApplication.java
+++ src/main/java/com/inovasoft/ms/emailService/EmailServiceApplication.java
@@ -2,6 +2,7 @@ package com.inovasoft.ms.emailService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
